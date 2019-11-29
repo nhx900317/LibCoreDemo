67ba6c7779b038c5e07b701ad39715e7848f2d6a
nhx <1018737497@qq.com>
2019-11-29 11:56:49 +0800

    MainActivity commit

--- app/src/main/java/com/thcb/libcoredemo/ui/MainActivity.kt
+++ app/src/main/java/com/thcb/libcoredemo/ui/MainActivity.kt
@@ -179,7 +179,7 @@ class MainActivity : MyBaseActivity(), LocationAb.LocationCallback, WeatherAb.We
     @SuppressLint("SetTextI18n")
     override fun onLocationFound(locationBean: LocationBean) {
         LogDebug.d(TAG, "onLocationFound:${locationBean.address}")
-        tvInfoShow!!.text = locationBean.address + locationBean.bearing
+        tvInfoShow!!.text = locationBean.address
         mCity = locationBean.city
     }
 
